0b640b4703fc24e539aa3eebe68eb3ec25457901
LionAB <lionelabatan@gmail.com>
2023-03-16 14:22:06 +0100

    Rooter setup

---
+++ src/views/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function HomePage() {
+    return (
+        <div id="home">
+            <h1 className="text-3xl text-sky-400 font-bold underline">Home Page</h1>
+        </div>
+    );
+}
