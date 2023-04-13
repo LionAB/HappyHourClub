5f95e4cec9aaf5e26e6b9f1c8b8ead3c8f29e3ff
Lucie Brackenier <lucie.brackenier@ynov.com>
2023-04-13 17:28:54 +0200

    search-bar design

--- src/Components/SearchBar/SearchBar.tsx
+++ src/Components/SearchBar/SearchBar.tsx
@@ -1,26 +1,32 @@
-import React, {useState, useContext, KeyboardEventHandler} from 'react'
+import React, { useState, useContext, KeyboardEventHandler } from 'react'
+import './SearchBar.css'
+
 import DrinkContext from '../../hooks/Context/DrinkContext'
 
 export default function SearchBar() {
 
-    const {fetchDrinkSearch} = useContext(DrinkContext)
+    const { fetchDrinkSearch } = useContext(DrinkContext)
     const [searchInput, setSearchInput] = useState("");
+
+/****************************************************************************************/
 
     const handleSearch = () => {
         fetchDrinkSearch(searchInput);
-      };
-    
+    };
+ 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
         if (event.key === "Enter") {
-          handleSearch();
+            handleSearch();
         }
-      };
+    };
+
+/****************************************************************************************/
 
     return (
         <div>
-            <input type="text" placeholder="Search" 
-            onChange={(event)=>setSearchInput(event.target.value)} 
-            onKeyDown={handleKeyDown}>
+            <input type="text" placeholder="Search"
+                onChange={(event) => setSearchInput(event.target.value)}
+                onKeyDown={handleKeyDown}>
             </input>
             <button onClick={handleSearch}>Search</button>
         </div>
