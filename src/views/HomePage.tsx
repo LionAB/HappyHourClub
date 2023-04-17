eb1f09dcc4dfed15b50aeffc8bcd2456887a0ac6
Lucie Brackenier <lucie.brackenier@ynov.com>
2023-04-17 15:16:54 +0200

    added dinamic animations on hover

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -47,7 +47,6 @@ export default function HomePage() {
     return (
         <div id="home">
             <h1 className="text-3xl text-sky-400 font-bold underline">Home Page</h1>
-            <div><Link to="/recipe">Navigate to Recipe</Link></div>
           
             <SearchBar/>
             
