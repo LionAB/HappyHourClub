5205cc4cd289bfd3769c3a03f688391456a26d17
LionAB <lionelabatan@gmail.com>
2023-04-19 15:49:29 +0200

    finito home no drinks

--- src/Components/CocktailCard/CocktailCard.tsx
+++ src/Components/CocktailCard/CocktailCard.tsx
@@ -3,30 +3,75 @@ import "./CocktailCard.css"
 import Drinks from "../../Models/Drink";
 import RecipeContext from '../../hooks/Context/RecipeContext';
 import { useNavigate } from 'react-router-dom';
+import DrinkContext from '../../hooks/Context/DrinkContext';
 
-export default function CocktailCard(props: {drink: Drinks | null}){
+export default function CocktailCard(props:{isnodrink:boolean|undefined,drink: Drinks | null}){
     const {fetchDrinkById}=useContext(RecipeContext);
-    const {drink} = props;
+    const {fetchAlcoholic,fetchBeer,fetchCoffee,fetchNonAlcoholic,fetchPunch,fetchShaker}=useContext(DrinkContext);
+    const {drink,isnodrink} = props;
     const navigate= useNavigate();
     const handleDrinkClick = (id:number|undefined) => {
-        if (id !==undefined){
+        if (isnodrink){
+            if (id !==undefined){
+                const stringId= id.toString()
+                switch (stringId) {
+                    case "1":
+                      fetchAlcoholic();
+                      break;
+                    case "2":
+                      fetchNonAlcoholic();
+                      break;
+                    case "3":
+                      fetchPunch();
+                      break;
+                    case "4":
+                      fetchBeer();
+                      break;
+                    case "5":
+                      fetchShaker();
+                      break;
+                    case "6":
+                      fetchCoffee();
+                      break;
+                    default:
+                      break;
+                  }
+                  
+                  navigate("/");
+            }
+        }else{
+            if (id !==undefined){
             const stringId= id.toString()
             fetchDrinkById(stringId);
             navigate("/recipe")
+        }
+        
             
             
         }
       };
 
     return(
-        <div key={drink?.id} className="card-container card-layout__item " onClick={()=>handleDrinkClick(drink?.id)}>
+        <>
+        {isnodrink ? 
+        (<><div key={drink?.id}  className="card-container card-layout__item " onClick={()=>handleDrinkClick(drink?.id)}>
+        <div className="card-image-container">
+            <img src={drink?.image}/>
+        </div>
+        <div className="cocktail-card-title">
+            <h1 className='cocktail-name'>{drink?.title}</h1>
+        </div>
+    </div></>):
+        ( <><div key={drink?.id}  className="card-container card-layout__item " onClick={()=>handleDrinkClick(drink?.id)}>
             <div className="card-image-container">
                 <img src={drink?.image}/>
             </div>
             <div className="cocktail-card-title">
                 <h1 className='cocktail-name'>{drink?.title}</h1>
             </div>
-        </div>
+        </div></>)}
+       
+        </>
     )
 }
 
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -21,6 +21,34 @@ export default function HomePage() {
     const { fetchCoffee } = useContext(DrinkContext);
     const { fetchPunch } = useContext(DrinkContext);
     const { fetchShaker } = useContext(DrinkContext);
+
+    const homedrink = [
+      {
+        id: 1,
+        image: "https://www.thecocktaildb.com/images/media/drink/xxyywq1454511117.jpg",
+        title: "Alcoholic beverages",
+      },{
+        id: 2,
+        image: "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+        title: "Non alcoholic beverages",
+      },{
+        id: 3,
+        image: "https://www.thecocktaildb.com/images/media/drink/9a4vqb1493067692.jpg",
+        title: "Punch",
+      },{
+        id: 4,
+        image: "https://www.thecocktaildb.com/images/media/drink/xuwpyu1441248734.jpg",
+        title: "Beer Beverages",
+      },{
+        id: 5,
+        image: "https://www.thecocktaildb.com/images/media/drink/rtwwsx1472720307.jpg",
+        title: "Shaker beverages",
+      },{
+        id: 6,
+        image: "https://www.thecocktaildb.com/images/media/drink/isql6y1487602375.jpg",
+        title: "Coffee beverages",
+      }
+    ]
     // Gets the data from thecocktaildb api and adapt it to our model
 /*     useEffect(() => {
       
@@ -58,11 +86,23 @@ export default function HomePage() {
             <SearchBar/>
             
             {drinks.length===0 ? (
-              <div> Pas de Drink</div>
+              <div className="nodrinks">
+                Some good sugestions to start ...
+                <div className="cardnodrinks-layout">
+                {homedrink?.map((mydrink)=>(
+                <CocktailCard 
+                  isnodrink={true}
+                  key={mydrink.id}
+                  drink={mydrink}
+                />
+            ))}
+                </div>
+              </div>
             ):(
             <div className="card-layout">
             {drinks?.map((mydrink)=>(
                 <CocktailCard 
+                isnodrink={false}
                 key={mydrink.id}
                 drink={mydrink}
                 />
